7fcb6d1c5d679ce16fae62b9974a4763530ba923
binlinh06 <nglinh150606@gmail.com>
2026-04-23 07:22:51 +0700

    Update MedicineRepository.java

--- src/main/java/org/example/it210_ss13_hw04/MedicineRepository.java
+++ src/main/java/org/example/it210_ss13_hw04/MedicineRepository.java
@@ -1,4 +1,26 @@
 package org.example.it210_ss13_hw04;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
 public class MedicineRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public List<Medicine> findExpiredMedicines(Date currentDate) {
+        Session session = sessionFactory.getCurrentSession();
+
+        String hql = "FROM Medicine m WHERE m.expiryDate < :currentDate";
+
+        Query<Medicine> query = session.createQuery(hql, Medicine.class);
+        query.setParameter("currentDate", currentDate);
+
+        return query.getResultList();
+    }
 }
